200ccda8e8191f0926e46a7bc837cbbee0cf2435
Max Karpawich <max.karpawich@gmail.com>
2020-06-16 14:55:07 -0400

    test: run separate tests for Next.js and Express support

---
+++ test/next.test.js
@@ -0,0 +1,29 @@
+/**
+ * Tests for the Next.js request object
+ */
+
+const runTests = require('./util')
+
+function getTestHttpRequest (textArgs) {
+  return {
+    'headers': {
+        'x-slack-request-timestamp': '1531420618',
+        'x-slack-signature': textArgs ? 'v0=a3e650d30d1e91901834f91d048c9d3c0a50e4dcffcef7bc67884e95df8588ce' : 'v0=a2114d57b48eac39b9ad189dd8316235a7b4a8d21a10bd27519666489c69b503',
+    },
+    'body': {
+      'token': 'xyzz0WbapA4vBCDEFasx0q6G',
+      'team_id': 'T1DC2JH3J',
+      'team_domain': 'testteamnow',
+      'channel_id': 'G8PSS9T3V',
+      'channel_name': 'foobar',
+      'user_id': 'U2CERLKJA',
+      'user_name': 'roadrunner',
+      'command': '/webhook-collect',
+      'text': textArgs || '',
+      'response_url': 'https://hooks.slack.com/commands/T1DC2JH3J/397700885554/96rGlfmibIGlgcZRskXaIFfN',
+      'trigger_id': '398738663015.47445629121.803a0bc887a14d10d2c447fce8b6703c'
+    },
+  }
+}
+
+runTests('Next.js', getTestHttpRequest)
